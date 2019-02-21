4ab0a096817464889a5a119bbe2ea18cc1947b8a
ToshUxanoff <tosha.uhanoff@yandex.ru>
2019-02-21 12:14:15 +0300

    added sigmoid func support

--- Lab1 /lab1.go
+++ Lab1 /lab1.go
@@ -34,7 +34,6 @@ type neuron struct {
 	learningRate  float64
 }
 
-//bug schroedingera
 func (n *neuron) netFunction(set [4]uint8) float64 {
 	var sum float64
 	for i, x := range set {
@@ -57,16 +56,27 @@ func thresholdFunction(x float64) float64 {
 	}
 	return 0
 }
-func expFunc(x float64) float64 {
+func sigmoidFunc(x float64) float64 {
 	arg := float64(-x)
 	return float64(1 / (1 + math.Exp(arg)))
 }
 
+func (n neuron) getMultiplier(set [4]uint8) float64 {
+	var result float64
+	if n.aFunctionName == "threshold" {
+		result = 1
+	} else if n.aFunctionName == "sigmoid" {
+		net := n.netFunction(set)
+		tmp := 1 - sigmoidFunc(net)
+		result = sigmoidFunc(net) * tmp
+	}
+	return result
+}
 func (n *neuron) correctWeight(delta int, set [4]uint8) {
-
+	multiplier := n.getMultiplier(set)
 	n.weights[0] = n.learningRate*float64(delta) + n.weights[0]
 	for i, x := range set {
-		n.weights[i+1] = n.learningRate*float64(delta)*float64(x) + n.weights[i+1]
+		n.weights[i+1] = n.learningRate*float64(delta)*float64(x)*multiplier + n.weights[i+1]
 	}
 }
 func printResults(results map[[4]uint8]uint8) {
@@ -79,7 +89,7 @@ func (n *neuron) train() {
 	i := 0 //epoch
 	for {
 		var results = map[[4]uint8]uint8{}
-		fmt.Println("Epoch:", i)
+		fmt.Println("\n------\nEpoch:", i)
 		fmt.Println("Weights:", n.weights)
 		summaryErr := 0
 		for set, idealValue := range trainingSet {
@@ -93,7 +103,6 @@ func (n *neuron) train() {
 		}
 		fmt.Println("Summary error:", summaryErr)
 		if summaryErr == 0 {
-			fmt.Println("Done!", n.weights)
 			printResults(results)
 			break
 		}
@@ -102,11 +111,22 @@ func (n *neuron) train() {
 }
 
 func main() {
-	a := float64(0 - 1)
-	fmt.Println(a, 0-1)
+	fmt.Println("start with threshold function")
 	n1 := neuron{weights: [5]float64{0, 0, 0, 0, 0}}
 	n1.aFunction = thresholdFunction
 	n1.aFunctionName = "threshold"
 	n1.learningRate = 0.3
 	n1.train()
+
+	fmt.Println("TEST: 0,0,1,0")
+	fmt.Println("RESULT:", n1.calculate([4]uint8{0, 0, 1, 0}))
+	fmt.Println("-----\nstart with sigmoid function")
+	n2 := neuron{weights: [5]float64{0, 0, 0, 0, 0}}
+	n2.aFunction = sigmoidFunc
+	n2.aFunctionName = "sigmoid"
+	n2.learningRate = 0.3
+	n2.train()
+
+	fmt.Println("TEST: 0,0,1,0")
+	fmt.Println("RESULT:", n2.calculate([4]uint8{0, 0, 1, 0}))
 }
